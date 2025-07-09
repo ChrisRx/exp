8aaae1e3c178902036cc750c6ca571be3fdb803b
Chris Marshall <chris@couch.life>
2025-07-09 11:37:06 -0400

    chore(sync): add Cond alias

--- sync/alias.go
+++ sync/alias.go
@@ -3,6 +3,10 @@ package sync
 import "sync"
 
 type (
+	// Cond is a type alias of [sync.Cond].
+	Cond = sync.Cond
+	// Locker is a type alias of [sync.Locker].
+	Locker = sync.Locker
 	// Map is a type alias of [sync.Map].
 	Map = sync.Map
 	// Mutex is a type alias of [sync.Mutex].
