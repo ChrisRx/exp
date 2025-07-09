fabc0109f056e6f8de4ca947a931802f9253b61e
Chris Marshall <chris@couch.life>
2025-07-09 11:30:41 -0400

    chore(sync): update docs

--- sync/alias.go
+++ sync/alias.go
@@ -3,23 +3,16 @@ package sync
 import "sync"
 
 type (
-	Locker    = sync.Locker
-	Map       = sync.Map
-	Mutex     = sync.Mutex
-	Once      = sync.Once
-	Pool      = sync.Pool
-	RWMutex   = sync.RWMutex
+	// Map is a type alias of [sync.Map].
+	Map = sync.Map
+	// Mutex is a type alias of [sync.Mutex].
+	Mutex = sync.Mutex
+	// Once is a type alias of [sync.Once].
+	Once = sync.Once
+	// Pool is a type alias of [sync.Pool].
+	Pool = sync.Pool
+	// RWMutex is a type alias of [sync.RWMutex].
+	RWMutex = sync.RWMutex
+	// WaitGroup is a type alias of [sync.WaitGroup].
 	WaitGroup = sync.WaitGroup
 )
-
-func OnceFunc(f func()) func() {
-	return sync.OnceFunc(f)
-}
-
-func OnceValue[T any](f func() T) func() T {
-	return sync.OnceValue(f)
-}
-
-func OnceValues[T1, T2 any](f func() (T1, T2)) func() (T1, T2) {
-	return sync.OnceValues(f)
-}
