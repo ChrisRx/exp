6231ea9c41ec38242582c42c6e88365d12ee334a
Chris Marshall <chris@couch.life>
2025-07-09 12:16:55 -0400

    fix(sync): must.Ok returns zero value on error

--- must/must.go
+++ must/must.go
@@ -2,10 +2,13 @@ package must
 
 import "log/slog"
 
-// Ok checks the error and logs using the default logger.
-func Ok[T any](v T, err error) T {
+// Ok takes a function that returns a value and error and returns only a
+// value. If an error is encountered, it is logged using the default logger and
+// the zero value of type T is returned.
+func Ok[T any](v T, err error) (zero T) {
 	if err != nil {
 		slog.Error(err.Error())
+		return zero
 	}
 	return v
 }
