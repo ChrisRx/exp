4bf7d2933f63150b4c719de48314858aa5206ac2
Chris Marshall <chris@couch.life>
2025-07-19 14:19:43 -0400

    feat(safe): Close only catches closed channel panic
    It may not even be possible to produce a panic outside of the close a
    closed channel panic, but mainly just removing the log print when
    closing a closed channel.

--- safe/chan.go
+++ safe/chan.go
@@ -1,14 +1,15 @@
 package safe
 
-import (
-	"go.chrisrx.dev/x/must"
-)
-
 // Close safely closes a Go channel.
-func Close[T any, C ~chan T](ch C) {
+func Close[T any, C ~chan T](ch C) (closed bool) {
 	if ch == nil {
 		return
 	}
-	defer must.Recover()
-	close(ch)
+	if err := Do(func() { close(ch) }); err != nil {
+		if err.Error() == "close of closed channel" {
+			return false
+		}
+		panic(err)
+	}
+	return true
 }
