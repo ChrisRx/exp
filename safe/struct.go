7366732693fee8ed487e9b2ce0d764e0a9eeda3e
Chris Marshall <chris@couch.life>
2025-07-30 17:07:47 -0400

    feat(safe): add NoTypeConversion

--- safe/struct.go
+++ safe/struct.go
@@ -1,5 +1,9 @@
 package safe
 
+// NoCompare is a type that can be added to the first field of a struct to
+// prevent that struct from being comparable.
+type NoCompare [0]func()
+
 // NoCopy implements [sync.Locker] which means that adding it to a struct
 // triggers the go vet copylocks check. This makes it useful to add to structs
 // that shouldn't be copied after first use:
@@ -15,3 +19,7 @@ type NoCopy struct{}
 
 func (*NoCopy) Lock()   {}
 func (*NoCopy) Unlock() {}
+
+// NoTypeConversion is a type that can be added as a field to a struct to
+// prevent unsafe type conversion.
+type NoTypeConversion[T any] [0]*T
