e2c38a57f69a262bb388f5cc4201ed0179c00dfd
Chris Marshall <chris@couch.life>
2025-08-02 10:30:50 -0400

    refactor: remove github.com/samber/lo

--- env/env.go
+++ env/env.go
@@ -2,14 +2,15 @@ package env
 
 import (
 	"github.com/caarlos0/env/v11"
-	"github.com/samber/lo"
 )
 
 // MustParse parses tags for the provided struct and sets values from
 // environment variables. It will only set values that have the `env` tag, and
 // if the tag is present it must be provided, otherwise this panics.
 func MustParse(v any) {
-	lo.Must0(env.ParseWithOptions(v, env.Options{
+	if err := env.ParseWithOptions(v, env.Options{
 		RequiredIfNoDef: true,
-	}))
+	}); err != nil {
+		panic(err)
+	}
 }
