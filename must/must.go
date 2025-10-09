846b76eb8f1f3385c386bd8e00515acb8fb583b7
Chris Marshall <chris@couch.life>
2025-10-09 13:16:56 -0400

    feat!(must): Ok should panic on error

--- must/must.go
+++ must/must.go
@@ -1,15 +1,11 @@
 // Package must provides functions for handling errors and panics.
 package must
 
-import "log/slog"
-
 // Ok takes a function that returns a value and error and returns only a
-// value. If an error is encountered, it is logged using the default logger and
-// the zero value of type T is returned.
+// value, otherwise it panics.
 func Ok[T any](v T, err error) (zero T) {
 	if err != nil {
-		slog.Error(err.Error())
-		return zero
+		panic(err)
 	}
 	return v
 }
