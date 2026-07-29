6bb7844828b5025368536c7e49b861c6647292f8
Chris Marshall <chris@couch.life>
2026-07-29 10:47:51 -0400

    feat(strings): Truncate

--- strings/strings.go
+++ strings/strings.go
@@ -149,3 +149,11 @@ func ToCamelCase(s string) string {
 	}
 	return b.String()
 }
+
+func Truncate(s string, n int) string {
+	runes := []rune(s)
+	if len(runes) > n {
+		return string(runes[:n])
+	}
+	return s
+}
